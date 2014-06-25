4dc9fe0a0bbb0d3caaec3ce89bbc3f64a30a6533
Kevin Jannis <me@janniskev.in>
2014-06-25 20:42:12 +0200

    Update boot.js

--- js/app/boot.js
+++ js/app/boot.js
@@ -10,4 +10,13 @@ Ember.Object.reopen.call(Ember.Object, Ember.I18n.TranslateableAttributes);
 var App = Ember.Application.create();
 App.Router.map(routes);
 
+App.MyRestAdapter = DS.RESTAdapter.reopen({
+  host: 'http://localhost',
+  namespace: 'api/v0.1'
+});
+
+App.Store = DS.Store.extend({
+	adapter: 'App.MyRestAdapter'
+});
+
 module.exports = App;
